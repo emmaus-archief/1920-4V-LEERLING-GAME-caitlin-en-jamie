c5615e2eb82d458d40b3e236b5c7b0255eede5df
Jamie13388 <Jamie13388@gmail.com>
2020-06-19 15:16:30 +0000

    timer klaar

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ var achtergrondImage;
 
 var spelerX = 200; // x-positie van speler
 var spelerY = 100; // y-positie van speler
-<<<<<<< HEAD
 var spelerImage;
 var vijandImageGroot;
 var vijandImageMiddel;
@@ -46,7 +45,9 @@ var vijandenKleinX = [];
 var vijandenKleinY = [];   
 var vijandenSnelheid = []; // horizontale snelheid van vijand
 
-var score = 0; // aantal behaalde punten
+var timerSec = 0; // aantal behaalde punten = behaalde tijd
+var timerMin = 0;
+var timerMiliSec = 0;
 
 function preload() {
     spelerImage = loadImage('afbeeldingen/plaatje_raket.png');
@@ -55,34 +56,6 @@ function preload() {
     vijandImageKlein = loadImage('afbeeldingen/asteroid_klein.png');
     achtergrond = loadImage('afbeeldingen/ruimte.jpg');
 }
-=======
-var spelerXSnelheid = 8;
-var spelerYSnelheid = 6;
-
-const SPEELVELDBREEDTE = 1280;
-const SPEELVELDHOOGHTE = 720;
-const SPEELVELDRANDBREEDTE = 20;
-const SPELERDIAMETER =80;
-
-const AANTALVIJANDEN = 10;
-const VIJANDDIAMETER = 40;
-var vijandenX = 200;
-var vijandenY = 100;
-var vijandenSnelheid = 2;
-var vijanden = 6;
-
-var stopwatchMin = 0; //stopwatch in min
-var stopwatchSec = 0; //stopwatch in sec 
-
-var score = 0; // aantal behaalde punten
-
-var img; 
-var img2;
-var backGroundImage;
-var loadImage;
-
-
->>>>>>> origin/master
 
 
 
@@ -110,26 +83,15 @@ var tekenStartscherm = function () {
  * Tekent het speelveld
  */
 var tekenVeld = function () {
-<<<<<<< HEAD
   
   rect(20, 20, width - 2 * 20, height - 2 * 20);
   image(achtergrond, 20, 20, width - 2 * 20, height - 2 * 20);
-=======
-  rect(backGroundImage, 20, 20, width - 2 * 20, height - 2 * 20);
->>>>>>> origin/master
 };
 
-function preload () {
-    img = loadImage('plaatjes/plaatje_raket.png');
-    img2 = loadImage('plaatje/asteroid.png');
-    backGroundImage = loadImage('plaatjes/space.png');
-    
-};
 
 /**
  * Tekent de vijand
  */
-<<<<<<< HEAD
 var tekenVijand = function() {
     for (var i = 0; i < vijandenGrootX.length; i++) {
         image(vijandImageGroot, vijandenGrootX[i], vijandenGrootY[i]);
@@ -200,13 +162,6 @@ function nieuweVijand() {
     vijandenKleinY.push(random(-500, -100));
     vijandenSnelheid.push(random(5, 13));
 }
-=======
-var tekenVijand = function(x, y) {
-    image(img2,50,50);
-};
-
-
->>>>>>> origin/master
 
 /**
  * Tekent en beweegt de speler
@@ -214,61 +169,22 @@ var tekenVijand = function(x, y) {
  * @param {number} y y-coördinaat
  */
 var tekenSpeler = function(x, y) {
-<<<<<<< HEAD
   fill("white");
   image(spelerImage, spelerX, spelerY);
 };
 
-=======
-  image(img, mouseX, mouseY);
-};
-
-/*
-function tekenTimer() {
-    var extraNul = ""
-    if (stopwatchSec < 10) {
-            extraNul = "0"
-    }
-
-    if (spelStatus === SPELEN) {
-        fill("white");
-        text(stopwatchMin + ":" + extraNul + stopwatchSec, 50, 50, 100, 100);
-    }
-
-    if (spelStatus === GAMEOVER) {
-        fill('white');
-        text(stopwatchMin + ";" + extraNul + stopwatchSec, 690, 380, 100, 100);
-    }
-
-} */
-
->>>>>>> origin/master
 /**
  * Beweegt speler met muis
  */
-<<<<<<< HEAD
 var beweegSpeler = function() {
     var muisXPos = mouseX;
     var muisYPos = mouseY;
-=======
-/*
-var beweegVijand = function() {
-    for (var i = 0; i < vijandenX.length; i++) {
-        vijandenY[i] = vijandenY[i] + vijandenSnelheid[i];
-
-        if (vijandenY[i] > SPEELVELDHOOGHTE + 20) {
-            geefVijandNieuwePositie(i)
-        }
-    }
-};
->>>>>>> origin/master
 
     var maxX = SPEELVELDBREEDTE - 80;
     var minX = 20;
     var maxY = SPEELVELDHOOGTE - 80;
     var minY = 20;
 
-<<<<<<< HEAD
     if (muisXPos > maxX) {
         muisXPos = maxX;
     }
@@ -297,36 +213,6 @@ var checkVijandGeraakt = function() {
 return false;
 };
 
-=======
-/**
- * Kijkt wat de toetsen/muis etc zijn.
- * Updatet globale variabele spelerX en spelerY
- */
-var beweegSpeler = function() {
-
-};
-
-/*
-function genereerVijanden() {
-    for(var i = 0; i < AANTALVIJANDEN; i++) {
-        vijanden[i] = new Enemy (random(20, 1100), random(20, 150), random(2, 7));
-    }
-} */
-
-/*
-function respawnVijand(){
-    for(var i = 0; i < vijanden.length; i++) {
-        vijanden[i].drawAndMove();
-        if(vijanden[i].isBuitenCanvas()) {
-            vijanden[i] = new Enemy (random(20, 1100), random(20, 150), random(2, 7));
-        }
-        if(vijanden[i].raaktSpeler()) {
-            spelerHP--;
-            vijanden[i] = new Enemy (random(20, 1100), random( 20 ,150), random(2, 7));
-        }
-    }
-} */
->>>>>>> origin/master
 
 /**
  * Zoekt uit of de speler is geraakt
@@ -348,22 +234,30 @@ var checkGameOver = function() {
   return false;
 };
 
-/*
-function updateTimer() {
-    stopwatchSec++;
 
-    if (stopwatchSec == 60) {
-        stopwatchMin++;
-        stopwatchSec = 0;
+var tekenTimer = function() {
+    timerMiliSec++;
+
+    if (timerMiliSec == 60) {
+        timerMiliSec = 0;
+        timerSec++;
     }
-}
+    if (timerSec == 60) {
+        timerSec = 0;
+        timerMin++;
+    }
 
-function geefVijandNieuwePositie(nummer) {
-    vijandenX[nummer] = (random(20, SPEELVELDBREEDTE - 20));
-    vijandenY[nummer] = (random(-250, -30));
-    vijandenSnelheid[nummer] = (random(2, 10));
-}
-*/
+    
+    fill('white');
+    textSize(35);
+    text(timerMin.toString(), 90, 100, 900, 750);
+    text(":", 110, 100, 900, 750);
+    text(timerSec.toString(), 125, 100, 900, 750);
+    text(":", 155, 100, 900, 750);
+    text(timerMiliSec.toString(), 170, 100, 900, 750);
+
+};
+
 
 /**
  * setup
@@ -372,24 +266,6 @@ function geefVijandNieuwePositie(nummer) {
  */
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-<<<<<<< HEAD
-=======
-  createCanvas(SPEELVELDBREEDTE, SPEELVELDHOOGHTE);
-
-  /*
-  for (var i =0; i < AANTALVIJANDEN; i++ ) {
-      geefVijandNieuwePositie(i);
-  }
-
-  setInterval(updateTimer, 1000);
-
-  console.log(vijandenX);
-  console.log(vijandenSnelheid);
-
-  */
- 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
->>>>>>> origin/master
   background('blue');
   createCanvas(SPEELVELDBREEDTE, SPEELVELDHOOGTE);
 
@@ -411,7 +287,7 @@ function setup() {
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
-/*
+
 function draw() {
   switch (spelStatus) {
     case STARTSCHERM:
@@ -419,11 +295,11 @@ function draw() {
         fill('white');
         textSize(35);
         text('Druk op ENTER om te starten', 325, 200, SPEELVELDBREEDTE - 270, 300);
-        text('Elke seconde is een punt waard', 325, 100, 800, 100)
+        text('Elke seconde is een punt waard!', 325, 100, 800, 100);
         textFont('Courier New');
         textSize(100);
         text('METEOR GARDEN', 440, SPEELVELDHOOGTE / 2 + 25, 600, SPEELVELDHOOGTE / 2 + 200);
-        fill(51,51,255)
+        fill(51,51,255);
         if (keyIsPressed === true && keyCode === 13) {
             spelStatus = SPELEN;
         }
@@ -432,13 +308,13 @@ function draw() {
 
 
     case SPELEN:
-<<<<<<< HEAD
     beweegVijand();
     beweegSpeler();
     rect(0,0,SPEELVELDBREEDTE,SPEELVELDHOOGTE);
     tekenVeld();
     tekenSpeler();
     tekenVijand();
+    tekenTimer();
 
     
     if (checkGameOver()) {
@@ -452,36 +328,3 @@ function draw() {
   }
 
 
-=======
-      beweegVijand();
-      beweegSpeler();
-      
-      if (checkVijandGeraakt()) {
-        // punten erbij
-        // nieuwe vijand maken
-      }
-      
-      if (checkSpelerGeraakt()) {
-        // leven eraf of gezondheid verlagen
-        // eventueel: nieuwe speler maken
-      }
-
-      background(backGroundImage);
-      beweegSpeler(); 
-      respawnVijand();
-      tekenVeld();
-        tekenSpeler(spelerX, spelerY);
-        tekenTimer();
-      checkGameOver();
-
-      if (checkGameOver()) {
-        spelStatus = GAMEOVER;
-        background;
-        tekenTimer();
-        clearTimeout();
-      }
-      case GAMEOVER:
-      break; 
-  }
-} */
->>>>>>> origin/master
